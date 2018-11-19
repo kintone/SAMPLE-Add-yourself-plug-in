1cd8b7eb12b5c48b67c042cdc6fd813751da8e80
Naoto00 <naoto-nakazato@cybozu.co.jp>
2018-11-19 14:51:30 +0900

    Add LICENSE

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,8 @@
+/*
+MIT License
+Copyright (c) 2018 Cybozu
+https://github.com/kintone/SAMPLE-Add-yourself-plug-in/blob/master/LICENSE
+*/
 jQuery.noConflict();
 (function($, PLUGIN_ID) {
     'use strict';
