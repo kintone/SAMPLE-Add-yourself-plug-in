c0fa6f5fdb1706097b67af353b7f5ec54b1a828b
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2019-02-26 16:58:35 -0800

    Modify config.js to use kintone-config-helper

--- src/js/config.js
+++ src/js/config.js
@@ -9,78 +9,36 @@ jQuery.noConflict();
     // Get configuration settings
     var CONF = kintone.plugin.app.getConfig(PLUGIN_ID);
 
-    function setDropDownForSpace(rows) {
-        // Get information of each field
-        for (var i = 0; i < rows.length; i++) {
-            var fields = rows[i];
-            for (var cnt = 0; cnt < fields.length; cnt++) {
-                var rowField = fields[cnt];
-                var $option = $('<option>');
-                switch (rowField.type) {
-                    // Only select Space fields
-                    case 'SPACER':
-                        $option.attr('value', rowField.elementId);
-                        $option.text(rowField.elementId);
-                        $('#select_space_field').append($option.clone());
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-        // Set default value
-        $('#select_space_field').val(CONF.space);
-    }
-    function getLayout() {
+    function setDropDown() {
         // Retrieve field information, then set drop-down
-        return kintone.api(kintone.api.url('/k/v1/preview/app/form/layout', true), 'GET',
-            {'app': kintone.app.getId()}).then(function(resp) {
-            var rows = [];
-            for (var i = 0; i < resp.layout.length; i++) {
-                var row = resp.layout[i];
-                // If type is ROW
-                if (row.type === 'ROW') {
-                    rows.push(row.fields);
-                } else if (row.type === 'GROUP') {
-                    for (var j = 0; j < row.layout.length; j++) {
-                        var row2 = row.layout[j];
-                        if (row2.type !== 'ROW') {
-                            continue;
-                        }
-                        rows.push(row2.fields);
-                    }
-                }
-            }
-            return rows;
-        }, function(resp) {
-            return alert('Failed to retrieve layout information.');
-        });
-    }
-    function setDropDownForUser() {
-        // Retrieve field information, then set drop-down
-        return kintone.api(kintone.api.url('/k/v1/preview/app/form/fields', true), 'GET',
-            {'app': kintone.app.getId()}).then(function(resp) {
-
-            for (var key in resp.properties) {
-                if (!resp.properties.hasOwnProperty(key)) {
-                    continue;
-                }
-                var prop = resp.properties[key];
-                var $option = $('<option>');
-
-                switch (prop.type) {
-                    // Only select User Selection field
-                    case 'USER_SELECT':
-                        $option.attr('value', prop.code);
-                        $option.text(prop.label);
-                        $('#select_user_field').append($option.clone());
+        // eslint-disable-next-line no-undef
+        return KintoneConfigHelper.getFields().then(function(resp) {
+            var $userDropDown = $('#select_user_field');
+            var $spaceDropDown = $('#select_space_field');
+            for (var i = 0; i < resp.length; i++) {
+                var $option = $('<option></option>');
+                switch (resp[i].type) {
+                    case "USER_SELECT":
+                        $option.attr('value', resp[i].code);
+                        $option.text(resp[i].label);
+                        $userDropDown.append($option.clone());
+                        break;
+                    case "SPACER":
+                        $option.attr('value', resp[i].elementId);
+                        $option.text(resp[i].elementId);
+                        $spaceDropDown.append($option.clone());
                         break;
                     default:
                         break;
                 }
             }
             // Set default values
-            $('#select_user_field').val(CONF.user);
+            if (CONF.user) {
+                $userDropDown.val(CONF.user);
+            }
+            if (CONF.space) {
+                $spaceDropDown.val(CONF.space);
+            }
         }, function(resp) {
             return alert('Failed to retrieve fields information');
         });
@@ -93,9 +51,8 @@ jQuery.noConflict();
         }
         $('#text-button-label').val(CONF.label);
         // Set drop-down list
-        setDropDownForUser()
-            .then(getLayout)
-            .then(setDropDownForSpace);
+        setDropDown();
+
         // Set input values when 'Save' button is clicked
         $('#check-plugin-submit').click(function() {
             var config = [];
